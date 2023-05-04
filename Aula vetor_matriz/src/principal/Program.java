67cac2cf9ff87148bfa872ee2f6d066da987e86a
Luan Muruk <murukluan@gmail.com>
2023-05-04 18:26:18 -0300

    atualizando a aula de 04/05, usando array e matriz

---
+++ Aula vetor_matriz/src/principal/Program.java
@@ -0,0 +1,66 @@
+package principal;
+
+public class Program {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		/*int[] numero = new int[5];
+		numero[0] = 100;
+		numero[1] = 20;
+		numero[2] = 15;
+		numero[3] = 55;
+		numero[4] = 66;
+		
+		System.out.println(numero[2] +" "+ numero[3]);
+		
+		int[] numeros = {1, 2, 3, 10, 50, 60, 80};
+		
+		System.out.println(numeros[3]);
+		System.out.println(numeros.length);
+		
+		for(int contador = 0; contador < numeros.length; contador++) {
+			System.out.println(numeros[contador]);
+		}
+		System.out.println("Foreach");
+		for(int indice : numeros) {
+			System.out.println(indice);
+		}
+		
+		int[][] mult = new int[3][3];
+		int contador = 0;
+		for(int i = 0; i < mult.length; i++) {
+			for(int j = 0; j < mult.length; j++ ) {
+				mult[i][j] = contador;
+				System.out.print(mult[i][j] + "\t");
+				contador++;
+			}
+			System.out.println();
+		}
+		
+		int[][] matriz = new int[3][3];
+		matriz[0][0] = 1;
+		matriz[0][1] = 2;
+		matriz[0][2] = 3;
+		matriz[1][0] = 4;
+		matriz[1][1] = 5;
+		matriz[1][2] = 6;
+		matriz[2][0] = 7;
+		matriz[2][1] = 8;
+		matriz[2][2] = 9;
+
+		for (int i = 0; i < matriz.length; i++) {
+		    for (int j = 0; j < matriz[i].length; j++) {
+		        System.out.print(matriz[i][j] + " ");
+		    }
+		    System.out.println();
+		}*/
+		
+		int media = 0;
+		media = (int) 8.5;
+		double media2 = 0;
+		media2 = 10;
+
+	}
+
+}
